27f36af63eea5212783edfa9d19dade2fe9d058a
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-07-13 23:54:08 -0300

    refactor: remove global config

--- server/apps/posts/src/posts.module.ts
+++ server/apps/posts/src/posts.module.ts
@@ -4,6 +4,8 @@ import { PostsController } from './posts.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { PostsRepository } from './posts.repository';
 import { PostDocument, PostSchema } from './models/post.schema';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -12,6 +14,13 @@ import { PostDocument, PostSchema } from './models/post.schema';
       { name: PostDocument.name, schema: PostSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [PostsController],
   providers: [PostsService, PostsRepository],
